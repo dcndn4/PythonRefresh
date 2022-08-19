5d9bc5ff2a6e188cc1413d9b9819994ee014f7a5
dcndn4 <stokes@iphouse.com>
2022-08-19 05:17:01 -0500

    another

---
+++ August scratch paper.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 19 05:03:46 2022
+
+@author: CS_Knit_tinK_SC
+"""
+
+# reviewing
+
+import pandas as pd
+
+df = pd.DataFrame({"Car": ['Car 1', 'Car 2', 'Car 3', 'Car 4', 'Car 5'], 
+                   "Model": ['Nissan', 'Nissan', 'Toyota', 'Ford', 'Checkers'],
+                   "Status": ['Purchased', 'Sold', 'Borrowed', 'Blue', 'Rented']})
+
+print(df)
+
+#%%
+
+df['Occur'] = df[df['Status'] == 'Sold'].groupby('Car')['Model'].transform('size')
+
+#%%
+
+filtered_df=df.loc[df['Occur'] ==1][['Car', 'Model', 'Status']]
+
+print(filtered_df)
