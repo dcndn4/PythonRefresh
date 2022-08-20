2a1c67760de05f96a645f5100ba1cf630130d4f7
dcndn4 <stokes@iphouse.com>
2022-08-20 06:13:52 -0500

    more python for me!

--- August scratch paper.py
+++ August scratch paper.py
@@ -31,6 +31,46 @@ print(filtered_df)
 
 x = {'a': 1, 'b':2}
 y = {'b': 3, 'c': 4}
+
+#%%
 z = {**x, **y}
+
+# this z = syntax was proposed in PEP 448 and in Python 3.5, per S.O. (sigh)
+# is in 'what's new in Python 3.5 document
+#%%
+
+z = {**x, 'foo': 1, 'br': 2, **y}
+print(z)
+
+#%%
+
+from timeit import repeat
+from itertools import chain
+
+x = dict.fromkeys('abcdefg')
+y = dict.fromkeys('efghijk')
+
+#%%
+
+def merge_two_dicts(x, y):
+    z = x.copy()
+    z.update(y)
+    return z
+
+#%%
+
+print(min(repeat(lambda: {**x, **y})))
+#%%
+print(min(repeat(lambda: merge_two_dicts(x, y))))
+#%%
+
+print(min(repeat(lambda: {k: v for d in (x, y) for k, v in d.items()})))
+
+#%%
+
+print(min(repeat(lambda: dict(chain(x.items(), y.items())))))
+#%%
+
+print(min(repeat(lambda: dict(item for d in (x, y) for item in d.items()))))
 
 #%%
